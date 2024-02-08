3dce05c84143534b3737464787de120d1e1ca6a2
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2024-02-08 21:49:20 +0100

    improve GTmetrix

--- script/animation.js
+++ script/animation.js
@@ -1,7 +1,7 @@
 //now animate each character into place from 100px above, fading in:
 export function loadAnim() {
   const tl = gsap.timeline({ default: { duration: 1 } });
-  tl.to('body', { visibility: visible })
+  tl.from('body', { autoAlpha: 0 })
     .from(
       'header',
       {
